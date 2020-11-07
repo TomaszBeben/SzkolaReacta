c953e5542e01e00a0af52bda6e773e9b81be12fa
TomaszBeben <tbeben@st.swps.edu.pl>
2020-11-07 14:40:04 +0100

    week-2 lesson-8

--- Project/src/App.js
+++ Project/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 // const News = (props) => {
@@ -14,35 +14,49 @@ import './App.css';
 const orange = '#e67e22';
 
 const styles = {
-    backgroundColor: '#ecf0f1',
-    border: '#1abc9c 1px solid',
-    padding: 10,
-    p: {
-      background: orange
-    }
-}
-
-// const News = (props) => {
-//   const { header, intro } = props;
-//   return (
-//     <div style={styles}>
-//       <h2>{header}</h2>
-//       <p style={styles.p}>{intro}</p>
-//     </div>
-//   )
-// }
-
-class News extends React.Component {
-  render(){
-    const{header, intro} = this.props;
-    return(
-      <div style={styles}>
-       <h2>{header}</h2>
-       <p style={styles.p}>{intro}</p>
-     </div>
-    )
+  backgroundColor: '#ecf0f1',
+  border: '#1abc9c 1px solid',
+  padding: 10,
+  p: {
+    background: orange
   }
 }
+
+const News = (props) => {
+  const { header, intro } = props;
+  const [score, setScore] = useState(5);
+  // setScore(10);
+  return (
+    <div style={styles}>
+      <h2>{header}</h2>
+      <p style={styles.p}>{intro}</p>
+      <p>{score}</p>
+    </div>
+  )
+}
+
+// class News extends React.Component {
+//   state = {
+//     date: new Date(),
+//     score: 5
+//   }
+//   componentDidMount() {
+//     this.setState({ score: 6 })
+//   }
+//   render() {
+//     const { header, intro } = this.props;
+//     const { score, date } = this.state;
+
+//     return (
+//       <div style={styles}>
+//         <h2>{header}</h2>
+//         <p style={styles.p}>{intro}</p>
+//         <p>{score}</p>
+//         <p>{date.getUTCHours()}:{date.getMinutes()}:{date.getSeconds()}</p>
+//       </div>
+//     )
+//   }
+// }
 
 const data = [
   {
