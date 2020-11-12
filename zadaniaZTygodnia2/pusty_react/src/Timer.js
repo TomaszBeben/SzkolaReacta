f8a2937c479402f7e5aaeac92ca4b2ca056a74ec
TomaszBeben <tbeben@st.swps.edu.pl>
2020-11-12 22:07:20 +0100

    week-2 task-3

--- zadaniaZTygodnia2/pusty_react/src/Timer.js
+++ zadaniaZTygodnia2/pusty_react/src/Timer.js
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 
-
+let sec = 0;
 const Timer = () => {
-    let sec = 0;
     const [time, setTime] = useState(0);
-    setInterval(() => {
-        return setTime(sec + 1);
-    }, 1000)
+
+    useEffect(() => {
+        setInterval(() => {
+            return setTime(sec += 1);
+        }, 1000)
+    }, [])
 
     return (
-    <h2>{time}</h2>
+        <h2>{`${time}s`}</h2>
     )
 }
 
